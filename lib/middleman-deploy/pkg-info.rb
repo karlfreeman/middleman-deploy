a5d8d9c4f3c91e5d0acf222776b688618ba18426
Tom Vaughan <tom@software6.net>
2013-07-29 19:58:39 -0400

    bump

--- lib/middleman-deploy/pkg-info.rb
+++ lib/middleman-deploy/pkg-info.rb
@@ -1,7 +1,7 @@
 module Middleman
   module Deploy
     PACKAGE = "middleman-deploy"
-    VERSION = "0.1.0"
+    VERSION = "0.1.1"
     TAGLINE = "Deploy a middleman built site over rsync, ftp, sftp, or git (e.g. gh-pages on github)."
   end
 end
